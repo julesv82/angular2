79c8fccda9d4c16dd843ead6e5e0d5c63534708c
Julian <julian.veling@atos.net>
2016-04-19 10:50:09 +0100

    workspace

--- services/dev/boot.ts
+++
@@ -1,7 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {AppComponent} from "./app.component";
-import {LoggingService} from "./services/logging.service";
-import {DataService} from "./services/data.service";
-
-//noinspection TypeScriptValidateTypes
-bootstrap(AppComponent, [DataService]);
